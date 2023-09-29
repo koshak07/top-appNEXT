6fdd5967f569bd834928c61e43ec8d514e7fb4f0
Vladimir <nochnoibriz@gmail.com>
2023-09-29 14:51:53 +0600

    add component Tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Htag } from "@/components";
+import { Htag } from "../components";
 import React from "react";
-import { Button } from "@/components";
+import { Button } from "../components";
+import { P } from "../components";
 export default function Home(): JSX.Element {
   return (
     <>
@@ -11,6 +12,9 @@ export default function Home(): JSX.Element {
         Кнопка
       </Button>
       <Button appearance='ghost'>Кнопка</Button>
+      <P size='s'>Маленький</P>
+      <P size='m'>Cредний</P>
+      <P size='l'>Большой</P>
     </>
   );
 }
